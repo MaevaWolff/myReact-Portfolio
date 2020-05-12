cbd5e906a16e85e4e01ef7aa031e7e5633c828e5
Maeva Wolff <maeva.wolff@hetic.net>
2020-05-12 15:14:10 +0200

    add loader component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,11 +2,13 @@ import React, { Component } from "react";
 
 // components
 import Main from "../../components/Main/Main";
+import Loader from "../../components/Loader/Loader";
 
 class Home extends Component {
   render() {
     return (
       <div>
+        <Loader></Loader>
         <Main></Main>
       </div>
     );
