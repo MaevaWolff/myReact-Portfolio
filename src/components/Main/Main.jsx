7324e382f04356e5a4a96998f37741144ff70f95
Maeva Wolff <maeva.wolff@hetic.net>
2020-05-13 15:17:05 +0200

    add class on loader on click

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,9 +1,13 @@
 import React, { Component } from "react";
+
 import "./Main.scss";
 
+// rooter
+
 // components
-import Header from "../../components/Header/Header";
-import Hero from "../../components/Hero/Hero";
+import Header from "../Header/Header";
+import Hero from "../Hero/Hero";
+// import Loader from "../Loader/Loader";
 
 class Main extends Component {
   render() {
